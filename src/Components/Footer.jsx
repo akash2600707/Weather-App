43339a2116895497822083e2f78e12e50338b6cb
AKASH <59521490+akash2600707@users.noreply.github.com>
2023-12-11 23:08:35 +0530

    Initial commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "../assets/favicon.png";
+function Footer() {
+  return (
+    <footer className="bg-purple-800 p-1 text-white text-center">
+      <div className="flex justify-center items-center">
+        <img
+          src={"https://i.ibb.co/kgzS0cv/logo.jpg"} 
+          alt="Logo"
+          className="mr-1 w-6 h-6"
+        />
+        <span className="font-bold">Weather-App</span>
+      </div>
+      <div className="mt-2">
+        <a href="https://akashr.me" className="mx-2">
+          Contact Us
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
+
+
+
+
